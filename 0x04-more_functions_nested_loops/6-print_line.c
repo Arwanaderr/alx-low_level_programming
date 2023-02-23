77e70b06a75bd5677d96aff46672a1ffdf4bc1a1
Arwanaderr <arwahalawa.cg@gmail.com>
2023-02-23 13:57:09 -0800

    commit suicide

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
- * print_line -draws a straight line in the terminal
- * @n: the number of times that the special character must be printed
- *
- * Return: a straight line
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-
-	int a = 1;
-
-	while (a <= n)
+	if (n <= 0)
 	{
-		_putchar(95);
-		a++;
-	}
 		_putchar('\n');
-	_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
