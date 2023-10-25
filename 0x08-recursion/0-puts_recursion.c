d9e1172d48cc4e6b9ca889d7386faf902468bf6f
arwanaderr <arwahalawa.cg@gmail.com>
2023-10-25 15:19:31 -0700

    print string followed by a line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+
+
+#include <unistd.h>
+void _puts_recursion(char *s);
+{
+}
+void print_all_char*s_starting_from(char *s)
+{
+if (c =="" )
+{return;}
+print_char*s;
+print_char( '\n');
+}
+int main(void)
+{
+print_string("");
+print_char('\n')
+return (0);
+}
